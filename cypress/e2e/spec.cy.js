d173669046b5148d9548baf4308bb65d65233f23
Lasitha Wijenayake <lasithdilshan20@gmail.com>
2023-06-15 13:23:03 +0800

    @lasithdilshan20 - Added custom command

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,16 +1,42 @@
 // enables intelligent code completion for Cypress commands
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
-Cypress.Commands.add("checkElement", { prevSubject: true }, ($subject) => {
-  if ($subject.length > 0) {
-    return cy.wrap($subject);
-  } else {
-    cy.log("Element not present. Skipping action.");
-  }
+Cypress.Commands.add('checkElement', { prevSubject: false }, (selector) => {
+  cy.log('**checkElement**')
+  return cy.get('body',{log: false}).then(($body) => {
+    const element = $body.find(selector);
+    const found = element.length > 0;
+
+    if (found) {
+      return cy.wrap(element,{log: false});
+    } else {
+      const callingCommand = ``;
+      const logMessage = `Element ${selector} not present. Command skipped. ${callingCommand}`;
+      Cypress.log({
+        name: 'customGet',
+        message: logMessage,
+        displayName: 'Custom Get'
+      });
+    }
+  });
 });
+
+
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('cypress/index.html')
-    cy.get('#fname1').checkElement().type('London')
+
+    cy.checkElement('#myButton').then(($element) => {
+      cy.wrap($element).click();
+    });
+
+    cy.checkElement('#fname1').then(($element) => {
+      cy.wrap($element).type('Alexis');
+    });
+
+    cy.checkElement('#lname').then(($element) => {
+      cy.wrap($element).type('Texas');
+    });
+
   })
 })
